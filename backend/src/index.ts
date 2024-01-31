8a871d7d573d52ba20ca167de473184c4b631779
mmednoui <gm_noui@esi.dz>
2024-01-31 02:47:02 +0100

    setup the env

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "hello from express" });
+});
+
+app.listen(7000, () => {
+  console.log("server running on localhost:7000");
+});
