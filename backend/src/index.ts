9972eac22b0d8f5e324eab7a861a7b912e0c8593
mmednoui <gm_noui@esi.dz>
2024-02-01 01:56:29 +0100

    add signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import authRouter from "./routes/auth.route";
 
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING as string)
@@ -15,9 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello from express" });
-});
+
+app.use("/api/auth", authRouter);
 
 app.listen(7000, () => {
   console.log("server running on localhost:7000");
