5c772c4149a1eaa60a8dccc3c68004b0f6b8d43d
mmednoui <gm_noui@esi.dz>
2024-02-05 19:13:18 +0100

    complete the search ui and func

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SearchBar from "./components/SearchBar";
 interface Props {
   children: React.ReactNode;
 }
@@ -8,6 +9,9 @@ const Layout = ({ children }: Props) => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
+      <div className="container mx-auto">
+        <SearchBar />
+      </div>
       <div className="container mx-auto py-10 flex-1">{children}</div>
     </div>
   );
--- client/src/api-client.ts
+++ client/src/api-client.ts
@@ -1,4 +1,4 @@
-import { HotelType } from "../../backend/src/types/types";
+import { HotelSearchResponse, HotelType } from "../../backend/src/types/types";
 import { LoginFormData } from "./pages/SignIn";
 import { RegisterFormData } from "./pages/SignUp";
 
@@ -106,6 +106,52 @@ export const fetchMyHotels = async (): Promise<HotelType[]> => {
   const response = await fetch(`${API_BASE_URL}/api/user/hotels`, {
     credentials: "include",
   });
+
+  if (!response.ok) {
+    throw new Error("Error fetching hotels");
+  }
+
+  return response.json();
+};
+
+export type SearchParams = {
+  destination?: string;
+  checkIn?: string;
+  checkOut?: string;
+  adultCount?: string;
+  childCount?: string;
+  page?: string;
+  facilities?: string[];
+  types?: string[];
+  stars?: string[];
+  maxPrice?: string;
+  sortOption?: string;
+};
+
+export const searchHotels = async (
+  searchParams: SearchParams
+): Promise<HotelSearchResponse> => {
+  const queryParams = new URLSearchParams();
+  queryParams.append("destination", searchParams.destination || "");
+  queryParams.append("checkIn", searchParams.checkIn || "");
+  queryParams.append("checkOut", searchParams.checkOut || "");
+  queryParams.append("adultCount", searchParams.adultCount || "");
+  queryParams.append("childCount", searchParams.childCount || "");
+  queryParams.append("page", searchParams.page || "");
+
+  queryParams.append("maxPrice", searchParams.maxPrice || "");
+  queryParams.append("sortOption", searchParams.sortOption || "");
+
+  searchParams.facilities?.forEach((facility) =>
+    queryParams.append("facilities", facility)
+  );
+
+  searchParams.types?.forEach((type) => queryParams.append("types", type));
+  searchParams.stars?.forEach((star) => queryParams.append("stars", star));
+
+  const response = await fetch(
+    `${API_BASE_URL}/api/search/hotels?${queryParams}`
+  );
 
   if (!response.ok) {
     throw new Error("Error fetching hotels");
